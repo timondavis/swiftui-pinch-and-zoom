18c6101c1599d3fcf78f686d7397d630bfa6d9c5
Timon Davis <timon.a.davis@gmail.com>
2022-12-28 22:17:27 -0800

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Timon Davis on 12/28/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

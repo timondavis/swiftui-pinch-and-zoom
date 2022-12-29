02277066ba57754efebd19f399da721dd7119a79
Timon Davis <timon.a.davis@gmail.com>
2022-12-28 23:52:50 -0800

    Tutorial used unexplained workaround to adjust component layout.  This is reworked so that logical structure is used to assign component position (instead of using Color.clear to address layout properties)

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -25,51 +25,53 @@ struct ContentView: View {
     // MARK: - CONTENT
     var body: some View {
         NavigationView {
-            ZStack {
+            VStack {
                 
-                Color.clear
-                
-                // MARK: - PAGE IMAGE
-                Image("magazine-front-cover")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .cornerRadius(10)
-                    .padding()
-                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
-                    .opacity(isAnimating ? 1 : 0)
-                    .offset(x: imageOffset.width, y: imageOffset.height)
-                    .animation(.linear(duration: 1), value: isAnimating)
-                    .scaleEffect(imageScale)
-                // MARK: - 1. TAP GESTURE
-                    .onTapGesture(count: 2, perform: {
-                        if (imageScale == 1) {
-                            withAnimation(.spring()) {
-                                imageScale = 5
-                            }
-                        } else {
-                            resetImageState();
-                        }
-                    })
-                // MARK: - 2. DRAG GESTURE
-                    .gesture(
-                        DragGesture()
-                            .onChanged { value in
-                                withAnimation(.linear(duration: 1)) {
-                                    imageOffset = value.translation
+                ZStack {
+                    
+                    // MARK: - PAGE IMAGE
+                    Image("magazine-front-cover")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(10)
+                        .padding()
+                        .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                        .opacity(isAnimating ? 1 : 0)
+                        .offset(x: imageOffset.width, y: imageOffset.height)
+                        .animation(.linear(duration: 1), value: isAnimating)
+                        .scaleEffect(imageScale)
+                    // MARK: - 1. TAP GESTURE
+                        .onTapGesture(count: 2, perform: {
+                            if (imageScale == 1) {
+                                withAnimation(.spring()) {
+                                    imageScale = 5
                                 }
+                            } else {
+                                resetImageState();
                             }
-                            .onEnded { _ in
-                                if (imageScale <= 1) {
-                                   resetImageState()
+                        })
+                    // MARK: - 2. DRAG GESTURE
+                        .gesture(
+                            DragGesture()
+                                .onChanged { value in
+                                    withAnimation(.linear(duration: 1)) {
+                                        imageOffset = value.translation
+                                    }
                                 }
-                            }
-                    )
-            } //: ZSTACK
-            .navigationTitle("Pinch & Zoom")
-            .navigationBarTitleDisplayMode(.inline)
-            .onAppear(perform: {
-                isAnimating = true;
-            })
+                                .onEnded { _ in
+                                    if (imageScale <= 1) {
+                                       resetImageState()
+                                    }
+                                }
+                        )
+                } //: ZSTACK
+                .navigationTitle("Pinch & Zoom")
+                .navigationBarTitleDisplayMode(.inline)
+                .onAppear(perform: {
+                    isAnimating = true;
+                })
+            } //: VSTACK
+            .frame(maxHeight: .infinity)
             //: MARK - INFO PANEL
             .overlay(
                 InfoPanelView(scale: imageScale, offset: imageOffset)
